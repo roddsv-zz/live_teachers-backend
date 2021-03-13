341881c4812b1399c33366d049b68643148a0b4e
Rodrigo Soares <adsrodsoares@gmail.com>
2021-03-13 15:22:45 -0300

    Projeto Criado

---
+++ src/main/java/com/rodrigo/liveteachers_back/LiveTeachersBackApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.liveteachers_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTeachersBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveTeachersBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/liveteachers_back/LiveTeachersBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.liveteachers_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveTeachersBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
