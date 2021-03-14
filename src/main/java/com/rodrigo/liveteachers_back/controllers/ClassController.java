454c634059a5fa0bc0cbb42da5bbb171a49d676a
Rodrigo Soares <adsrodsoares@gmail.com>
2021-03-13 23:29:02 -0300

    POST /classes

--- src/main/java/com/rodrigo/liveteachers_back/controllers/ClassController.java
+++ src/main/java/com/rodrigo/liveteachers_back/controllers/ClassController.java
@@ -1,12 +1,16 @@
 package com.rodrigo.liveteachers_back.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigo.liveteachers_back.dto.ClassDTO;
 import com.rodrigo.liveteachers_back.services.ClassService;
@@ -23,6 +27,14 @@ public class ClassController {
 	public ResponseEntity<List<ClassDTO>> findAll(){
 		List<ClassDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClassDTO> insert(@RequestBody ClassDTO dto){
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto); 
+		
 	}
 
 }
--- src/main/java/com/rodrigo/liveteachers_back/services/ClassService.java
+++ src/main/java/com/rodrigo/liveteachers_back/services/ClassService.java
@@ -1,5 +1,6 @@
 package com.rodrigo.liveteachers_back.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rodrigo.liveteachers_back.dto.ClassDTO;
+import com.rodrigo.liveteachers_back.dto.StudentDTO;
+import com.rodrigo.liveteachers_back.dto.TeacherDTO;
 import com.rodrigo.liveteachers_back.entities.Class;
+import com.rodrigo.liveteachers_back.entities.Student;
+import com.rodrigo.liveteachers_back.entities.Teacher;
 import com.rodrigo.liveteachers_back.repositories.ClassRepository;
+import com.rodrigo.liveteachers_back.repositories.StudentRepository;
+import com.rodrigo.liveteachers_back.repositories.TeacherRepository;
 
 @Service
 public class ClassService {
@@ -17,10 +24,33 @@ public class ClassService {
 	@Autowired
 	private ClassRepository repository;
 	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Autowired
+	private TeacherRepository teacherRepository;	
+	
 	@Transactional(readOnly = true)
 	public List<ClassDTO> findAll() {
 		List<Class> list = repository.findAll();
 		return list.stream().map(x -> new ClassDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public ClassDTO insert(ClassDTO dto) {
+		Class newClass = new Class(null, dto.getSubject(), dto.getNumberOfHours(), Instant.now());
+		for (StudentDTO s : dto.getStudents()) {
+			Student student = studentRepository.getOne(s.getId());
+			newClass.getStudents().add(student);
+		}
+		for (TeacherDTO t : dto.getTeachers()) {
+			Teacher teacher = teacherRepository.getOne(t.getId());
+			newClass.getTeachers().add(teacher);
+		}
+		newClass = repository.save(newClass);
+		
+		return new ClassDTO(newClass);
+	}
+	
 
 }
